ea87ba903d8bc64e6c2ea51c4072a27e953aca80
tclhaddy <tclhaddy@iastate.edu>
2018-04-13 18:02:09 -0500

    Graph
    Made a private class Graph

--- NetworkInfluence.java
+++ NetworkInfluence.java
@@ -13,74 +13,87 @@ import java.util.*;
 
 public class NetworkInfluence
 {
+	
+	/**
+	 *  Private class that represents a graph to be used by NetworkInfluence
+	 */
+	private class Graph {
+		
+		private LinkedList<String> adjList[];
+		
+		private Graph(String graphData) throws FileNotFoundException {
+			adjList = makeAdjList(graphData);
+		}
+		
+		private LinkedList<String>[] makeAdjList(String path) throws FileNotFoundException {
+			
+			File file = new File(path);
+			Scanner scan = new Scanner(file);
+			
+			int vertices = 0;
+			String line;
+			line = scan.nextLine();
+			try {
+				vertices = Integer.parseInt(line);
+			} catch (NumberFormatException e) {
+				System.out.println("File did not have the number of vertices on the first line");
+			}
+			
+			LinkedList<String> result[] = new LinkedList[vertices];
+			for (int i = 0; i < result.length; ++i) {
+	            result[i] = new LinkedList<>();
+	        }
+			
+			int pos = 0;
+			while (scan.hasNextLine()) {
+				line = scan.nextLine();
+				Scanner s = new Scanner(line);
+				while (s.hasNext()) {
+					String word = s.next();
+					for (int i = 0; i < result.length; i++) {
+						
+						//First word exists already as a vertex, add second word as an edge
+						if (word.equals(result[i].get(0))) {
+							word = s.next();
+							result[i].addFirst(word);
+						}
+					}
+					
+					//Otherwise there is no vertex for the first word, make one
+					result[pos].addFirst(word);
+					pos++;
+				}
+				
+				pos++;
+				s.close();
+			}
+			scan.close();
+			return result;
+		}
+	}
+	
 	private String filepath;
-	private LinkedList<String> adjList[];
+	private Graph graph;
 	
 	public NetworkInfluence(String graphData)
 	{
 		filepath = graphData;
 		try {
-			adjList = makeAdjList();
+			graph = new Graph(filepath);
 		} catch (FileNotFoundException e) {
-			System.out.println("Filepath was not found");
+			System.out.println("File to contruct the graph was not found");
 		}
 	}
 	
-	private LinkedList<String>[] makeAdjList() throws FileNotFoundException {
-		
-		File file = new File(filepath);
-		Scanner scan = new Scanner(file);
-		
-		int vertices = 0;
-		String line;
-		line = scan.nextLine();
-		try {
-			vertices = Integer.parseInt(line);
-		} catch (NumberFormatException e) {
-			System.out.println("File did not have the number of vertices on the first line");
-		}
-		
-		LinkedList<String> result[] = new LinkedList[vertices];
-		for (int i = 0; i < result.length; ++i) {
-            result[i] = new LinkedList<>();
-        }
-		
-		int pos = 0;
-		while (scan.hasNextLine()) {
-			line = scan.nextLine();
-			Scanner s = new Scanner(line);
-			while (s.hasNext()) {
-				String word = s.next();
-				for (int i = 0; i < result.length; i++) {
-					
-					//First word exists already as a vertex, add second word as an edge
-					if (word.equals(result[i].get(0))) {
-						word = s.next();
-						result[i].addFirst(word);
-					}
-				}
-				
-				//Otherwise there is no vertex for the first word, make one
-				result[pos].addFirst(word);
-				pos++;
-			}
-			
-			pos++;
-			s.close();
-		}
-		scan.close();
-		return result;
-	}
-
 	public int outDegree(String v)
 	{
 		int total = 0;
 		
-		//Goes through each vertice in adjList
-		for(int i=0;i<adjList.length;i++){
+		//Goes through each vertex in adjList
+		for(int i=0;i<graph.adjList.length;i++){
 			
 			//If the first word is the same as the vertice, add 1 to total
-			if(adjList[i].get(0).equals(v))
+			if(graph.adjList[i].get(0).equals(v))
 				total++;
 		}
 		
@@ -103,26 +116,25 @@ public class NetworkInfluence
 		return result2;
 	}
 	
-	
 	// Helper Method to find shortest path
-	public ArrayList<String> shortestPathHelper(String from, String to, ArrayList<String> visited){
+	private ArrayList<String> shortestPathHelper(String from, String to, ArrayList<String> visited){
 		ArrayList<String> out = new ArrayList<String>();
 		visited.add(from);
 		
 		//Goes through the adjList to find all connected vertices
-		for(int i=0;i<adjList.length;i++){
-			if(adjList[i].get(0).equals(from)){
+		for(int i=0;i<graph.adjList.length;i++){
+			if(graph.adjList[i].get(0).equals(from)){
 				boolean hasVisited = false;
 				
 				//Checks if the found edge has already been visited
 				for(int j=0;j<visited.size();j++){
-					if(adjList[i].get(1)==visited.get(j)){
+					if(graph.adjList[i].get(1)==visited.get(j)){
 						hasVisited = true;
 						break;
 					}
 				}
 				if(hasVisited == false){
-					out.add(adjList[i].get(1));
+					out.add(graph.adjList[i].get(1));
 				}
 			}
 		}
